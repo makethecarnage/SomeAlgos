cc2f791d3678b55121e0da7552d9ec0399fc2ca5
Христолюбов Роман <rakhristolyubov@miem.hse.ru>
2022-07-07 20:52:29 +0300

    added qsort

---
+++ first/qsort.py
@@ -0,0 +1,15 @@
+def quicksort(array):
+    if len(array) < 2:
+        return array
+    else:
+
+        pivot = array[0]
+        less = [i for i in array[1:] if i <= pivot]
+        greater = [i for i in array[1:] if i > pivot]
+
+        return quicksort(less) + [pivot] + quicksort(greater)
+
+if __name__ == '__main__':
+
+    array = [4,2,7,5,9]
+    print(quicksort(array))
