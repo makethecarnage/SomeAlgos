f3da8a663b0f12eb2d5d3609a4f95e36da977a25
Христолюбов Роман <rakhristolyubov@miem.hse.ru>
2022-07-07 22:00:25 +0300

    just one more

--- first/qsort.py
+++ first/qsort.py
@@ -1,9 +1,13 @@
+import random
+
+
 def quicksort(array):
     if len(array) < 2:
         return array
     else:
 
         pivot = array[0]
+
         less = [i for i in array[1:] if i <= pivot]
         greater = [i for i in array[1:] if i > pivot]
 
@@ -11,5 +15,5 @@ def quicksort(array):
 
 if __name__ == '__main__':
 
-    array = [4,2,7,5,9]
+    array = [4,2,7,5,9,1,6,3]
     print(quicksort(array))
